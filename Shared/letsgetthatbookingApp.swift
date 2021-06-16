15e9c83760bd06931bb95657ab91c6f3bfa3b69c
JwoodCoding <81912221+JwoodCoding@users.noreply.github.com>
2021-06-16 21:31:04 +0100

    Initial Commit

---
+++ Shared/letsgetthatbookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  letsgetthatbookingApp.swift
+//  Shared
+//
+//  Created by James Woodward on 16/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct letsgetthatbookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
